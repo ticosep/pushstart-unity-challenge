420f1b873211527a5bc1b13a38ca121f1b0bdc73
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2020-02-02 15:39:13 -0300

    Add game manager and buildings prefabs

---
+++ Assets/Prefabs/Building.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+
+    public int price;
+    public int rentability;
+    public int population_happiness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(GameManager.money);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
